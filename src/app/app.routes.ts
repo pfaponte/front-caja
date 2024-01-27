1733200f5aa8654d396d9a727b5039bed1373e10
Pedro <fernandoaponte1103@gmail.com>
2024-01-27 06:29:27 -0500

    Init project front caja

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ClientComponent } from './pages/client/client.component';
+import { AccountComponent } from './pages/account/account.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { PagesComponent } from './pages/pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',
+        component: PagesComponent,
+        children: [
+            { path : 'dashboard', component: DashboardComponent },
+            { path : 'client', component: ClientComponent },
+            { path : 'account', component: AccountComponent },
+            { path : '', redirectTo: '/dashboard', pathMatch: 'full'},
+        ]    
+    },
+
+    { path : 'login', component: LoginComponent },
+    { path : 'register', component: RegisterComponent },
+
+    { path : '**', component: PagenotfoundComponent },
+];
---
+++ src/app/pages/client/client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './client.component.html',
+  styles: ``
+})
+export class ClientComponent {
+
+}
